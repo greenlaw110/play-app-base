9a332080a75ce38cd1f91b9ac4ac657ab7c8c78e
Green Luo <greenlaw110@gmail.com>
2012-12-28 11:11:52 +1100

    change D method to static

--- src/com/greenlaw110/utils/D.java
+++ src/com/greenlaw110/utils/D.java
@@ -5,20 +5,20 @@ import org.joda.time.DateTime;
 import java.util.Date;
 
 public class D {
-    public Date plusDays(Date date, int day) {
+    public static Date plusDays(Date date, int day) {
         DateTime dt = new DateTime(date);
         dt = dt.plusDays(day);
         return dt.toDate();
     }
-    public Date minusDays(Date date, int day) {
+    public static Date minusDays(Date date, int day) {
         return plusDays(date, -1 * day);
     }
-    public Date plusMonths(Date date, int month) {
+    public static Date plusMonths(Date date, int month) {
         DateTime dt = new DateTime(date);
         dt = dt.plusMonths(month);
         return dt.toDate();
     }
-    public Date minusMonths(Date date, int month) {
+    public static Date minusMonths(Date date, int month) {
         return plusMonths(date, -1 * month);
     }
 }
